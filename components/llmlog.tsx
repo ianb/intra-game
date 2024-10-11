1fc7b40767554e6a8517e3f15475e35e8c987e2e
Ian Bicking <ianb@colorstudy.com>
2024-10-11 17:11:52 -0500

    Add a clear logs function, and make it so you can click to jump to the response in the logs

--- components/llmlog.tsx
+++ components/llmlog.tsx
@@ -1,26 +1,46 @@
 import { logSignal } from "@/lib/llm";
 import { GeminiChatType, LlmLogType } from "@/lib/types";
 import { useSignal } from "@preact/signals-react";
-import { useEffect } from "react";
+import { forwardRef, useEffect, useRef } from "react";
 
 export default function LlmLog() {
   return (
     <div className="text-xs">
-      {logSignal.value.map((log) => (
-        <LogItem log={log} key={log.request.meta.index} />
+      {logSignal.value.map((log, i) => (
+        <LogItem log={log} first={i === 0} key={log.request.meta.index} />
       ))}
     </div>
   );
 }
 
-function LogItem({ log }: { log: LlmLogType }) {
-  const hide = useSignal(false);
+function LogItem({ log, first }: { log: LlmLogType; first: boolean }) {
+  const responseRef = useRef<HTMLPreElement>(null);
+  const hide = useSignal<boolean | null>(null);
+  const actuallyHide = hide.value === null ? !first : hide.value;
   return (
     <div className="border-b border-gray-700 p-2">
       <div
+        className="float-right cursor-pointer opacity-75 hover:opacity-100 hover:bg-blue-600 px-3"
+        onClick={() => {
+          if (responseRef.current) {
+            hide.value = false;
+            responseRef.current.scrollIntoView({ behavior: "smooth" });
+          } else if (hide.value || hide.value === null) {
+            hide.value = false;
+            setTimeout(() => {
+              if (responseRef.current) {
+                responseRef.current.scrollIntoView({ behavior: "smooth" });
+              }
+            }, 100);
+          }
+        }}
+      >
+        ↓
+      </div>
+      <div
         className="bg-blue-900 text-white p-1 cursor-default"
         onClick={() => {
-          hide.value = !hide.value;
+          hide.value = !actuallyHide;
         }}
       >
         #{log.request.meta.index} {log.request.meta.title}{" "}
@@ -28,13 +48,13 @@ function LogItem({ log }: { log: LlmLogType }) {
         {log.request.model === "gemini-1.5-flash" && " ⚡"}
       </div>
       <LlmError log={log} />
-      {!hide.value && (
+      {!actuallyHide && (
         <>
           <LlmRequest
             request={log.request}
             finished={!!log.response || !!log.errorMessage}
           />
-          <LlmResponse response={log.response} />
+          <LlmResponse ref={responseRef} response={log.response} />
         </>
       )}
     </div>
@@ -84,16 +104,21 @@ function LlmRequestItem({ role, text }: { role: string; text: string }) {
   );
 }
 
-function LlmResponse({ response }: { response?: string }) {
-  if (!response) {
-    return null;
+const LlmResponse = forwardRef<HTMLPreElement, { response?: string }>(
+  ({ response }, ref) => {
+    if (!response) {
+      return null;
+    }
+    return (
+      <pre
+        ref={ref}
+        className="whitespace-pre-wrap text-white bg-green-900 -indent-2 pl-2"
+      >
+        <strong>model:</strong> {response}
+      </pre>
+    );
   }
-  return (
-    <pre className="whitespace-pre-wrap text-white bg-green-900 -indent-2 pl-2">
-      <strong>model:</strong> {response}
-    </pre>
-  );
-}
+);
 
 function RequestTime({ start, end }: { start: number; end?: number }) {
   if (end) {
@@ -119,4 +144,12 @@ function timeElapsed(ms: number) {
   const minutes = Math.floor(seconds / 60);
   const secondsPadded = (seconds % 60).toString().padStart(2, "0");
   return `${minutes}:${secondsPadded}`;
+}
+
+export function clearLogs() {
+  console.log("clear");
+  logSignal.value = logSignal.value.filter(
+    (log) => log.end === undefined && !log.errorMessage
+  );
+  console.log("done", logSignal.value);
 }
